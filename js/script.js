96708c94d1c64c1f67a8cd96c65958b61007f231
Andrea Miglietta <m.andrea96@hotmail.it>
2022-09-01 13:14:23 +0200

    palindrome

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log('JS OK!!!')
+
+// write a word
+const word = prompt('Write a word..');
+
+// is this word palindrome?
+const isPalindrome = checkPalindromeWord(word);
+
+console.log('Is palindrome: ', isPalindrome);
+
+
+function checkPalindromeWord(text) {
+    console.log(text);
+
+    let palindromeText = '';
+
+    for (let i = 0; i < text.length; i++) {
+        const userWord = text.charAt(i);
+        palindromeText = userWord + palindromeText;
+    }
+    console.log(palindromeText);
+
+    const sameWord = palindromeText === text;
+
+    return sameWord;
+}
