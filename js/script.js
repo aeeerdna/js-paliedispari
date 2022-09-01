3cf9364a0e279d706b94f8ac58d73136821fe56c
Andrea Miglietta <m.andrea96@hotmail.it>
2022-09-01 13:16:52 +0200

    comments

--- js/script.js
+++ js/script.js
@@ -8,12 +8,13 @@ const isPalindrome = checkPalindromeWord(word);
 
 console.log('Is palindrome: ', isPalindrome);
 
-
+// function check. is palindrome? true || false
 function checkPalindromeWord(text) {
     console.log(text);
 
     let palindromeText = '';
 
+    // for loop
     for (let i = 0; i < text.length; i++) {
         const userWord = text.charAt(i);
         palindromeText = userWord + palindromeText;
